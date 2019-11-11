eada3225e492fdc354657943c1788625ea960f8c
Bronson Brown <Brsn90@icloud.com>
2019-11-11 15:15:09 -0700

    Add files via upload

---
+++ reverse-string.js
@@ -0,0 +1,11 @@
+//
+// This is only a SKELETON file for the 'Reverse String' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export function reverseString(string) { 
+  let newString = string.split('').reverse().join('') 
+  return newString;
+} 
+
+
