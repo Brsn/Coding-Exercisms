5deb0dab54a943e4b03d1d96d48e74475b966060
Bronson Brown <Brsn90@icloud.com>
2019-11-22 09:34:42 -0700

    Add files via upload
    Cleaned up raindrop exercism

--- raindrops.js
+++ raindrops.js
@@ -2,46 +2,25 @@
 // This is only a SKELETON file for the 'Raindrops' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
- //var temp = "";
- 
- export const convert = (num) =>  {
- //temp = num.toString();
- if(divByThree(num) && divByFive(num) && divBySeven(num)) {
- return ("PlingPlangPlong");
- }
- if(divByThree(num) && divByFive(num)) {
- return ("PlingPlang");
- }
- if(divByThree(num) && divBySeven(num)){
- return ("PlingPlong");
- }
- if(divByThree(num)) {
-   return "Pling";
- }
- if(divByFive(num) && divBySeven(num)) {
- return ("PlangPlong");
- }
- if(divByFive(num)) {
- return ("Plang");
- }
- if(divBySeven(num)){
- return ("Plong");
- }
- else return num.toString();
-}
+//var temp = "";
 
-function divByThree(num) {
-  if(num % 3 == 0) return true;
-  else return false;
-  
-}
-
- function divByFive(num) {
-  if(num % 5 == 0) return true;
-  else return false;
-}
-
-function divBySeven(num) {
-  if(num % 7 == 0) return true;
-  else return false;
-}
+export const convert = (num) => {
+	//temp = num.toString();
+	if (divByThree(num) && divByFive(num) && divBySeven(num)) return 'PlingPlangPlong';
+	if (divByThree(num) && divByFive(num)) return 'PlingPlang';
+	if (divByThree(num) && divBySeven(num)) return 'PlingPlong';
+	if (divByFive(num) && divBySeven(num)) return 'PlangPlong';
+	if (divByFive(num)) return 'Plang';
+	if (divBySeven(num)) return 'Plong';
+	if (divByThree(num)) return 'Pling';
+	else return num.toString();
+};
+const divByThree = (num) => {
+	return num % 3 == 0;
+};
+const divByFive = (num) => {
+	return num % 5 == 0;
+};
+const divBySeven = (num) => {
+	return num % 7 == 0;
+};
