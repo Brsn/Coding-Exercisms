eb593aca8c0e423a2af66516b958ffeea470053e
Bronson Brown <Brsn90@icloud.com>
2019-11-26 15:35:01 -0700

    Add files via upload

---
+++ binary-search-tree.js
@@ -0,0 +1,31 @@
+//
+// This is only a SKELETON file for the 'Binary Search Tree' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+class Node {
+	constructor(data, left, right) {
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+}
+class BinarySearchTree {
+	constructor(root) {
+		this.root = null;
+	}
+
+	get data() {
+		throw new Error('Remove this statement and implement this function');
+	}
+	get right() {
+		throw new Error('Remove this statement and implement this function');
+	}
+
+	get left() {
+		throw new Error('Remove this statement and implement this function');
+	}
+
+	insert() {
+		throw new Error('Remove this statement and implement this function');
+	}
+}
